d730b7370c81cd7dc024ec53fd556e0957a92eb7
phillip deleon <phillipdeleon68@gmail.com>
2024-11-05 10:02:32 -0600

    updated

--- Program.cs
+++ Program.cs
@@ -28,6 +28,36 @@
             Console.WriteLine($"Min value of {numInput} & {floatInput} = " + Math.Min(numInput, floatInput));
             Console.WriteLine($"Max value of {numInput2} & {floatInput2} = " + Math.Max(numInput2, floatInput2));
 
+         
+            //3.
+
+            Console.WriteLine();
+            Console.WriteLine("3.");
+            Console.WriteLine("Enter a number: ");
+            int numberInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            int numberInput2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = numberInput1 + numberInput2;
+
+            Console.WriteLine($"The sum of the 2 numbers entered is: {sum}. ({numberInput1} " +
+                $"+ {numberInput2} = {sum}) ");
+
+            //4.
+
+            Console.WriteLine();
+            Console.WriteLine("4.");
+            Console.WriteLine("Enter a number: ");
+            double numberInput12 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            double numberInput13 = Convert.ToDouble(Console.ReadLine());
+
+            double sum2 = numberInput12 / numberInput13;
+
+            Console.WriteLine($"{numberInput12} / {numberInput13} = {sum2}");
+
         }
     }
 }
