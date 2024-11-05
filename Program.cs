ec450613d3c16ab86460ff1c6c6405df75a97e51
phillip deleon <phillipdeleon68@gmail.com>
2024-11-05 10:11:10 -0600

    update

--- Program.cs
+++ Program.cs
@@ -11,53 +11,53 @@
             Console.WriteLine("2. Create a simple console application and declare basic datatypes like int, float, decimal, double and print their min and max values.");
             Console.WriteLine();
 
-            Console.WriteLine("Please enter a whole number (int): ");
+            Console.WriteLine("\tPlease enter a whole number (int): ");
             int numInput = Convert.ToInt32(Console.ReadLine());
 
             //float
-            Console.WriteLine("Please enter a 'float' number (ex: 3.14f):");
+            Console.WriteLine("\tPlease enter a 'float' number (ex: 3.14f):");
             float floatInput = Convert.ToSingle(Console.ReadLine());
 
             //int
-            Console.WriteLine("Please enter a whole number (int): ");
+            Console.WriteLine("\tPlease enter a whole number (int): ");
             int numInput2 = Convert.ToInt32(Console.ReadLine());
 
             //float
-            Console.WriteLine("Please enter a 'float' number (ex: 3.14f):");
+            Console.WriteLine("\tPlease enter a 'float' number (ex: 3.14f):");
             float floatInput2 = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine($"Min value of {numInput} & {floatInput} = " + Math.Min(numInput, floatInput));
-            Console.WriteLine($"Max value of {numInput2} & {floatInput2} = " + Math.Max(numInput2, floatInput2));
+            Console.WriteLine($"\t\t\t\tMin value of {numInput} & {floatInput} = " + Math.Min(numInput, floatInput));
+            Console.WriteLine($"\t\t\t\tMax value of {numInput2} & {floatInput2} = " + Math.Max(numInput2, floatInput2));
 
          
             //3.
 
             Console.WriteLine();
-            Console.WriteLine("3.");
-            Console.WriteLine("Enter a number: ");
+            Console.WriteLine("3. Write a C# Sharp program to print the sum of two numbers.");
+            Console.WriteLine("\tEnter a number: ");
             int numberInput1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter another number: ");
+            Console.WriteLine("\tEnter another number: ");
             int numberInput2 = Convert.ToInt32(Console.ReadLine());
 
             int sum = numberInput1 + numberInput2;
 
-            Console.WriteLine($"The sum of the 2 numbers entered is: {sum}. ({numberInput1} " +
+            Console.WriteLine($"\t\t\t\tThe sum of the 2 numbers entered is: {sum}. ({numberInput1} " +
                 $"+ {numberInput2} = {sum}) ");
 
             //4.
 
             Console.WriteLine();
-            Console.WriteLine("4.");
-            Console.WriteLine("Enter a number: ");
+            Console.WriteLine("4. Write a C# Sharp program to print the result of dividing two numbers.");
+            Console.WriteLine("\tEnter a number: ");
             double numberInput12 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Enter another number: ");
+            Console.WriteLine("\tEnter another number: ");
             double numberInput13 = Convert.ToDouble(Console.ReadLine());
 
             double sum2 = numberInput12 / numberInput13;
 
-            Console.WriteLine($"{numberInput12} / {numberInput13} = {sum2}");
+            Console.WriteLine($"\t\t\t\t{numberInput12} / {numberInput13} = {sum2}");
 
         }
     }
